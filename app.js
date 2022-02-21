c391b26a000c7f5bc51e1593569869f0ed8e0967
Lindsey Howard <miss_leading009@yahoo.com>
2022-02-21 10:17:10 -0500

    initial upload...

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+	"mongodb+srv://node-auth:7INPOhOQdbLDnmkv@node-blog.ik7aq.mongodb.net/node-blog?retryWrites=true&w=majority";
+mongoose
+	.connect(dbURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then((result) => app.listen(3000))
+	.catch((err) => console.log(err));
+
+// routes
+app.get("*", checkUser);
+app.get("/", (req, res) => res.render("home"));
+app.get("/milkshakes", requireAuth, (req, res) => res.render("milkshakes"));
+app.use(authRoutes);
