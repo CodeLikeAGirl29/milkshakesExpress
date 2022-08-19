a0ce210bbaf2d103fa723516c5b5afb8cc9cfd7f
Lindsey Howard <miss_leading009@yahoo.com>
2022-08-19 17:01:36 -0400

    error fixing

--- app.js
+++ app.js
@@ -14,20 +14,28 @@ app.use(cookieParser());
 // view engine
 app.set("view engine", "ejs");
 
+requireAuth("dotenv").config()
+
 // database connection
-const dbURI =
-	"mongodb+srv://node-auth:7INPOhOQdbLDnmkv@node-blog.ik7aq.mongodb.net/node-blog?retryWrites=true&w=majority";
+const dbURI = process.env.MONGODB_CONNECTION_STRING;
 mongoose
 	.connect(dbURI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useCreateIndex: true,
 	})
-	.then((result) => app.listen(3000))
-	.catch((err) => console.log(err));
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
 
 // routes
 app.get("*", checkUser);
 app.get("/", (req, res) => res.render("home"));
 app.get("/milkshakes", requireAuth, (req, res) => res.render("milkshakes"));
 app.use(authRoutes);
+
+// Establishing the port
+const PORT = process.env.PORT ||5000;
+ 
+// Executing the server on given port number
+app.listen(PORT, console.log(
+  `Server started on port ${PORT}`));
