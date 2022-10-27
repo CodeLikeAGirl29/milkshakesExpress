7157bbef3bd19085c0f6abb6ba501d6ab369a583
Lindsey Howard <miss_leading009@yahoo.com>
2022-10-27 02:39:13 -0400

    changes to app.js

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config()
 const express = require('express');
+const morgan = require("morgan");
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const methodOverride = require("method-override")
 const authRoutes = require('./routes/authRoutes.js');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/auth.js');
 
-dotenv.config();
 const app = express();
 
-// middleware
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")) //logging
+app.use(methodOverride("_method")) // override for put and delete requests from forms
+app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
@@ -16,23 +26,45 @@ app.use(cookieParser());
 // view engine
 app.set('view engine', 'ejs');
 
-// database connection
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
 const dbURI = process.env.MONGODB_URI;
-mongoose
-	.connect(dbURI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	})
-	.then((result) =>
-		app.listen(process.env.PORT || 5000, () => {
-			console.log('port 5000 running');
-		})
-	)
-	.catch((err) => console.log(err));
+const CONFIG = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}
 
-// routes
+// Establish Connection
+mongoose.connect(dbURI, CONFIG)
+// mongoose
+// 	.connect(dbURI, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 		useCreateIndex: true,
+// 	})
+// 	.then((result) =>
+// 		app.listen(process.env.PORT || 5000, () => {
+// 			console.log('port 5000 running');
+// 		})
+// 	)
+// 	.catch((err) => console.log(err));
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+.on("open", () => console.log("Connected to Mongoose"))
+.on("close", () => console.log("Disconnected from Mongoose"))
+.on("error", (error) => console.log(error))
+
+//////////////////////////////////////////////
+// Routes
+//////////////////////////////////////////////
 app.get('*', checkUser); //apply check user middleware to every single get request and shows user in the header
 app.use(authRoutes);
 app.get('/', (req, res) => res.render('home'));
 app.get('/milkshakes', requireAuth, (req, res) => res.render('milkshakes'));
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`))
